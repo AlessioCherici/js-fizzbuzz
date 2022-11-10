f79747be514faa3d649e59d9c3eb5c28d3de8cc6
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-10 17:19:52 +0100

    Creato javascript per prezzo *fine*

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let valuebut = document.getElementById("bottone");
+
+valuebut.addEventListener("click",function(){
+    let destinazione = document.getElementById("destinazione");
+    let eta = document.getElementById("etacliente");
+    let destinationValue = destinazione.value;
+    let etaValue= eta.value;
+    console.log(destinationValue);
+    console.log(etaValue);
+
+    let prezzo = (destinationValue * 0.21);
+
+        if (etaValue >= 65) {
+            prezzo = (prezzo - prezzo * 40 / 100);
+        }
+
+        else if (etaValue > 0 && etaValue < 18){
+            prezzo = (prezzo - prezzo * 20 / 100);
+        }
+
+        prezzo = prezzo.toFixed(2);
+        console.log(prezzo);
+
+        document.getElementById("costo").innerHTML = prezzo + "€" 
+
+});
+
+
